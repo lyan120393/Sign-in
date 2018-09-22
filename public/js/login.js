c36f5bbb695471e866543af2ddc6c6f12300a45c
LongyituoYan <ly@MBP.local>
2018-09-22 00:18:31 -0700

    finish new token graber

--- public/js/login.js
+++ public/js/login.js
@@ -39,7 +39,11 @@ document.querySelector("#login-form").addEventListener("submit", function(e) {
             //获取服务返回的 Header, 然后在服务器上获取 x-auth
             let header = e.target.getAllResponseHeaders();
             //死代码, 获取 x-auth
-            let token = header.slice(87, 258);
+            // let token = header.slice(87, 258);
+            let token = header.slice(228, 399);
+
+            // 从这里开始, 这里不可以使用死的数据去获取 token , 要再想想办法. 死数据在 heroku 上面获取到的内容不是 token 的内容, 导致服务器无法根据 token 去识别用户,  会导致 me.page 的运行错误,无法正常的获取数据.
+
             //把 token 放入到 hash 里面,方便跨页面使用同一个 token
             location.assign(`/me#${token}`);
             // visitme(token);
